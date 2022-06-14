734fa088b0cf49fff402eb3dc34fc5c1c75bbe0c
lelaranja <leticiajlaranja@gmail.com>
2022-06-14 16:30:54 -0300

    fix: ajuste de formatação arquivo html e js

--- script.js
+++ script.js
@@ -5,11 +5,15 @@ $("#submit").click(function () {
     url: `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${nmCity}?key=WAJVBFE8WPRUYZQ9KTFCEDE8K`,
     type: "GET",
     success: function (dado) {
-      console.log(dado);
-      $(".descriptionClimate").text(dado.description);
       $(".city").text(dado.address);
-      $(".temp").text(dado.days[0].temp);
-      $(".wind").text(dado.days[0].windspeed);
+      $(".temp").text((dado.days[0].temp) + ' ºF');
+      $(".wind").text('Velocidade do vento: '+ dado.days[0].windspeed + ' km/h');
+
+      switch(icon){
+        case $(".icon").text(dado.days[0].icon) = snow:
+          $(".icon").attr('src', )
+      }
+      
     },
     error: function (error) {
       console.log(error);
