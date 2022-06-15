80b5808c405c60086019ae5b402488a5a6a34142
lelaranja <leticiajlaranja@gmail.com>
2022-06-15 15:51:54 -0300

    feat: ajuste midia query e remoção console.log do js

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ $("#submit").click(function () {
     url: `http://api.weatherapi.com/v1/current.json?key=de5f8b0f21f64fdd8b2205228221406&q=${nmCity}`,
     type: "GET",
     success: function (dado) {
-      console.log(dado);
       $(".city").text(dado.location.name + ", " + dado.location.region);
       $(".icon").attr("src", dado.current.condition.icon);
       $(".icon").css({
