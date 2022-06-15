fe30546e5bcb8647a0f27aaafe0c22085f94035c
lelaranja <leticiajlaranja@gmail.com>
2022-06-15 12:09:37 -0300

    feat: ajuste de layout

--- script.js
+++ script.js
@@ -5,17 +5,19 @@ $("#submit").click(function () {
     url: `http://api.weatherapi.com/v1/current.json?key=de5f8b0f21f64fdd8b2205228221406&q=${nmCity}`,
     type: "GET",
     success: function (dado) {
-      console.log(dado)
-      $(".city").text(dado.location.name);
-      $(".temp").text(dado.current.temp_c+ ' ºC');
-      $('.icon').css({
-        'display': 'flex',
-        'justify-content': 'center',
-        'align-items': 'center'
-    });
-      $(".icon").attr('src', dado.current.condition.icon);
-      $('.precProb').text('Precipitação prevista: ' + dado.current.precip_mm + ' mm');
-      $(".wind").text('Velocidade do vento: '+ dado.current.wind_kph + ' km/h');
+      console.log(dado);
+      $(".city").text(dado.location.name + ", " + dado.location.region);
+      $(".icon").attr("src", dado.current.condition.icon);
+      $(".icon").css({
+        display: "inline",
+      });
+      $(".temp").text(dado.current.temp_c + " ºC");
+      $(".precProb").text(
+        "Precipitação prevista: " + dado.current.precip_mm + " mm"
+      );
+      $(".wind").text(
+        "Velocidade do vento: " + dado.current.wind_kph + " km/h"
+      );
     },
     error: function (error) {
       console.log(error);
